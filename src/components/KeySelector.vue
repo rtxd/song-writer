4b0a5c6d7dad6c91f42505cda265d4ca1cc16eb8
Alejandro Munoz <alex@digitalmaas.com>
2022-02-16 21:23:47 +1100

    install vuetify and prettier

---
+++ src/components/KeySelector.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <v-card color="red" class="mb-3 px-3">
+            <v-card-text> Hullo </v-card-text>
+        </v-card>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'View',
+}
+</script>
+
+<style></style>
