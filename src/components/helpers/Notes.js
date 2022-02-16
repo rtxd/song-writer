8dc49711502396689797d8ff6dab7855ec8f62b7
Alejandro Munoz <alex@digitalmaas.com>
2022-02-16 22:39:37 +1100

    fix getNotes

--- src/components/helpers/Notes.js
+++ src/components/helpers/Notes.js
@@ -1,15 +1,18 @@
 const notes = {
-    majors: ['A', 'B', 'C', 'D', 'E', 'F', 'G'],
+    letters: ['A', 'B', 'C', 'D', 'E', 'F', 'G'],
     minors: ['Am', 'Bm', 'Cm', 'Dm', 'Em', 'Fm', 'Gm'],
     flat: '♭',
     sharp: '#',
 }
 
-export function getNote(note, noteType, pitch) {
-    let noteAsText = ''
-    noteType === 'Major'
-        ? noteAsText.concat(notes.majors.find(note))
-        : noteAsText.concat(notes.minors.find(note))
-    if (pitch === 'sharp') noteAsText.concat(notes.sharp)
-    return noteAsText
+export default function getNote(note, pitch = null, noteType = 'major') {
+    let noteString = notes.letters.find((o) => o === note)
+    //add pitch
+    if (pitch) {
+        if (pitch === 'sharp') noteString = noteString + '#'
+        else noteString = noteString + '♭'
+    }
+    //add minor or keep it major
+    if (noteType === 'minor') noteString = noteString + 'm'
+    return noteString
 }
